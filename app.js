530bf472a323be9a1f8d6026cbc5665c721dd018
Quinn Ceplis <quinn.ceplis1@ucalgary.ca>
2020-03-23 23:00:59 -0700

    more post route updates

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require('express')
 app.use(express.json());
 app.use(require('./controllers'));
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
--- models/Post.js
+++ models/Post.js
@@ -1,3 +1,16 @@
-exports.newPost = () => {
+exports.newPost = (req) => {
+    const post = {
+        userId: userId,
+        postText: postText,
+        postImage: postImage
+    };
 
+    // call database handler
+
+    if (post.image) newImagePost(post.image);
+}
+
+// Do I need to export this if newPost is being exported?
+function newImagePost(image) {
+    // db handler
 }
