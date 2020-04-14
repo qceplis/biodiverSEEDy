121962c080f6c11ca1fb07b6d949abd60c5dcaae
Quinn Ceplis <quinn.ceplis1@ucalgary.ca>
2020-04-14 14:40:00 -0700

    fixed router

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -2,7 +2,7 @@ const express = require("express");
 var router = express.Router();
 
 router.use("/users", require("./userController"));
-//router.use("/posts", require("./postController"));
+router.use("/posts", require("./postController"));
 router.use("/seeds", require("./seedController"));
 
 module.exports = router;
